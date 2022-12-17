543d3f59f814dcfb9a7ef867d650bc28bfefb883
venkat-devish <venkatsuryakiranv@gmail.com>
2022-12-17 12:47:34 +0530

    add: case with object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import UseState_A from "./components/useStateHook/UseState_A";
+import UseState_C from "./components/useStateHook/UseState_C";
 
 function App() {
   return (
     <div className="App">
       <h1>CRASH USESTATE</h1>
-      <UseState_A />
+      <UseState_C />
     </div>
   );
 }
