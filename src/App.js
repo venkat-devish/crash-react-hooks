e89ea59db904650b21e4eb735f62f8f9aecf1c6c
venkat-devish <114286141+venkat-devish@users.noreply.github.com>
2022-12-18 21:38:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Counter_C from "./Hooks/useReducerHook/Counter_C";
 function App() {
   return (
     <div className="App">
-      <h1>CRASH USE CONTEXT HOOK</h1>
+      <h1>CRASH REACT HOOKS</h1>
       <Counter_C />
     </div>
   );
